578ba3209d5f76537750038c0de8b7ad591ea326
Josef Renner <josef.renner@gmail.com>
2014-08-20 23:07:33 +0200

    Start Customizing Admin and Change Names in Models
    Register categories and pages in admin.py. Add custom names to
    models, to improve readability in the admin interface.

--- admin.py
+++ admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from cat_nav.models import page_container
+from cat_nav.models import page_container, product_categories
 
-admin.site.register(page_container)
+class PageAdmin(admin.ModelAdmin):
+    fields = [('page_title', 'page_categories'), 'page_content']
+    list_display = ('page_title', 'page_categories')
+
+admin.site.register(page_container, PageAdmin)
+
+admin.site.register(product_categories) 
--- models.py
+++ models.py
@@ -13,13 +13,13 @@ class product_categories(models.Model):
 
 class page_container(models.Model):
     # Content of page and it's title. What max_length should be used here?
-    page_title = models.CharField(max_length=200)
+    page_title = models.CharField('Title', max_length=200)
     # TextField Info: https://docs.djangoproject.com/en/1.6/ref/models/fields/#django.db.models.TextField
-    page_content = models.TextField(max_length=1200)
+    page_content = models.TextField('Content', max_length=1200)
     # Number to be used to define order of listed page names
-    page_order = models.IntegerField(default=0)
+    page_order = models.IntegerField('Order of Page', default=0)
     # Category ("~ies"?)
-    page_categories = models.ForeignKey(product_categories)
+    page_categories = models.ForeignKey(product_categories, verbose_name="Category")
     # Links to Pages containing similar Information
     """
     Der nächste Punkt soll Links zu Seiten enthalten, die verwandte Produkte enthalten. Voraussichtlich ist hier der falsche Ort dafür. Ich gehe davon aus, dass es hierfür eine eigene Klasse brauchen wird.
@@ -30,6 +30,5 @@ class page_container(models.Model):
     In Python 3 __unicode__ will need to be replaced by __str__ (?)
     """
     def __unicode__(self):
-        """
         return self.page_title
         return self.page_content
