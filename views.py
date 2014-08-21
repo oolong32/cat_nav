da041435114ba0ef3ea2e5f4f400f5784466debf
Josef Renner <josef.renner@gmail.com>
2014-08-21 15:28:05 +0200

    Add Template for Category List View

--- views.py
+++ views.py
@@ -1,15 +1,15 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from cat_nav.models import product_categories
 
 def index(request):
     category_list = product_categories.objects.order_by('category_order')
-    output = ', '.join([p.category_name for p in category_list])
-    return HttpResponse(output)
+    context = {'category_list': category_list}
+    return render(request, 'cat_nav/index.html', context)
 
 def page_content_view(request, page_categories_id):
-    return HttpResponse("You're looking at page %s." % page_categories_id)
+    pass
+    # return HttpResponse("You're looking at page %s." % page_categories_id)
 
 # def category_list(request, page_categories_id):
 #     return HttpResponse("You're looking at the List of Categories %s." % page_categories_id)
