746212520410470b5efd54d517d499f6d64552bf
Josef Renner <josef.renner@gmail.com>
2014-08-21 10:14:41 +0200

    Add Comments for Admin Model

--- admin.py
+++ admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from cat_nav.models import page_container, product_categories
 
 class PageAdmin(admin.ModelAdmin):
-    fields = [('page_title', 'page_categories'), 'page_content']
+    # page container overview, show categories next to titles
     list_display = ('page_title', 'page_categories')
+    # page container edit page: show title and category in same field, then the content.
+    fields = [('page_title', 'page_categories'), 'page_content']
 
 admin.site.register(page_container, PageAdmin)
 
