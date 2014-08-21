dba946e9562cc30b64bea6f2055e0115d740913c
Josef Renner <josef.renner@gmail.com>
2014-08-21 15:01:15 +0200

    Output List of Categories

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from cat_nav import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<page_categories_id>\d+)/$', views.page_content_view, name='content'),
 )
 
--- views.py
+++ views.py
@@ -1,5 +1,18 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
+from cat_nav.models import product_categories
+
 def index(request):
-    return HttpResponse("Hello World! You're at the category navigation index.")
+    category_list = product_categories.objects.order_by('category_order')
+    output = ', '.join([p.category_name for p in category_list])
+    return HttpResponse(output)
+
+def page_content_view(request, page_categories_id):
+    return HttpResponse("You're looking at page %s." % page_categories_id)
+
+# def category_list(request, page_categories_id):
+#     return HttpResponse("You're looking at the List of Categories %s." % page_categories_id)
+
+# def page_list(request, page_categories_id)
+#     return HttpResponse("You're looking at the List of Pages in Category %s." % page_categories_id)
