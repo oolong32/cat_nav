c01ab6b3905f6f220fe69b34bc89ab567398b710
Josef Renner <josef.renner@gmail.com>
2014-08-22 12:04:11 +0200

    Add URL, View and Template for a Single Page Content
    Right now the url for the "details" view passes a "page_category_id"
    which so far is not needed in the template and a page_container_id
    whicht defines which object of the pag_container model is to be
    desplayed.
    This is not satisfying. Eventually the ID's should be replaced by
    category names and page titles. Unfortunately I have no bleeding clue
    how to capture these bleeding values in the urls.

--- urls.py
+++ urls.py
@@ -5,10 +5,11 @@ from cat_nav import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<page_categories_id>\d+)/$', views.products, name='products'),
+    url(r'^(?P<page_categories_id>\d+)/details/(?P<page_container_id>\d+)/$', views.details, name='details'),
 )
 """
-url(r'^(?P<page_categories_id>\d+)/details/$', views.details, name='details'),
-"""
-"""
 <page_categories_id> muss durch lesbare namen ersetzt werden. durch den "category_name hinter der page_categories_id.
+genauso sollte die referenz zum page_container objekt nicht durch eine id, sondern durch den namen angezeigt werden.
+dem aufmerksamen betrachter mag aufgefallen sein, dass die url fuer "details" fehlerhaft ist, es gibt keine referenz zu einem objekt in "page_container"
+Was passiert mit Bezeichnungen, die Underscores oder Spaces enthalten?
 """
--- views.py
+++ views.py
@@ -16,8 +16,7 @@ def products(request, page_categories_id):
     product = get_object_or_404 (product_categories, pk=page_categories_id)
     return render(request, 'cat_nav/products.html', {'product': product})
     
-def details(request, page_categories_id): 
-    pass
-    # return HttpResponse("You're looking at page %s." % page_categories_id)
-
+def details(request, page_categories_id, page_container_id): 
+    page = get_object_or_404(page_container, pk=page_container_id)
+    return render(request, 'cat_nav/content.html', {'page': page})
 
