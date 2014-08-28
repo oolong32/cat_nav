8b8aecb7784174d1a2ac0faf71fe269872d5acf3
Josef Renner <josef.renner@gmail.com>
2014-08-29 00:04:46 +0200

    Replace 'Index' View with Generic ListView
    As explained in the django documentation, example with Publishers,
    Authors and Books.

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from djago.views.generic import ListView
 from django.shortcuts import render, get_object_or_404 
 
 from cat_nav.models import product_categories, page_container
@@ -7,10 +8,13 @@ Die Views sollten vielleicht anders heissen, bzw. ist "cat-nav" in der URL unbef
 Ebenfalls unbefriedigend ist die anzeige von numerischen id's in der URL statt Namen von Kategorien und Produkten.
 """
 
-def index(request):
-    category_list = product_categories.objects.order_by('category_order')
-    context = {'category_list': category_list}
-    return render(request, 'cat_nav/index.html', context)
+class CategoryList
+    model = product_categories
+
+# def index(request):
+#     category_list = product_categories.objects.order_by('category_order')
+#     context = {'category_list': category_list}
+#     return render(request, 'cat_nav/index.html', context)
 
 def products(request, page_categories_id):
     product = get_object_or_404 (product_categories, pk=page_categories_id)
