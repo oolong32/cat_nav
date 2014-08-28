ad0e45288973281b67178485e125b5cf81a1843e
Josef Renner <josef.renner@gmail.com>
2014-08-28 23:21:51 +0200

    Minor Changes, Nothing of importance
    I mainly commit this to cleanup the repository. The most notable change
    is outputting some variables in the "list_products" template in order to
    enhance my understanding.

--- templatetags/list_products.py
+++ templatetags/list_products.py
@@ -1,2 +1,11 @@
 from django import template
+from cat_nav.models import page_container
 register = template.Library()
+
+def list_products(product):
+    # now how do I filter this?
+    # here or in the template?
+    product_list = page_container.objects.all
+    return {'product_list': product_list}
+
+register.inclusion_tag('cat_nav/list_products.html')(list_products)
