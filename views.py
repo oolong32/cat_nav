875ce7d5ac6647aa79178ff10f6a94c5f848e366
Josef Renner <josef.renner@gmail.com>
2014-08-29 22:58:12 +0200

    Remove Obsolete Views
    Removed the original index view (replaced by a ListView).
    As there is no more need for a separate column for a product list (which
    moved into sublists of the category list), the product view has been
    commented out.

--- views.py
+++ views.py
@@ -11,14 +11,9 @@ Ebenfalls unbefriedigend ist die anzeige von numerischen id's in der URL statt N
 class CategoryList(ListView):
     model = product_categories
 
-# def index(request):
-#     category_list = product_categories.objects.order_by('category_order')
-#     context = {'category_list': category_list}
-#     return render(request, 'cat_nav/index.html', context)
-
-def products(request, page_categories_id):
-    product = get_object_or_404 (product_categories, pk=page_categories_id)
-    return render(request, 'cat_nav/products.html', {'product': product})
+# def products(request, page_categories_id):
+#     product = get_object_or_404 (product_categories, pk=page_categories_id)
+#     return render(request, 'cat_nav/products.html', {'product': product})
     
 def details(request, page_categories_id, page_container_id): 
     page = get_object_or_404(page_container, pk=page_container_id)
