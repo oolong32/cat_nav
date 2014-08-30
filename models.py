96e14e928265409f2cb0cc9204e2b1bf3b952ce0
Josef Renner <josef.renner@gmail.com>
2014-08-30 22:33:35 +0200

    Remove obsolete comments
    Some comments were obsolete. Others needed to be moved to the To Do list
    in the README

--- models.py
+++ models.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-# achtung, könnte verkehrt aufgegleist sein. mir ist nicht klar, ob sich kategorien auf seiteninhalte beziehen müssen oder umgekehrt. verglichen mit dem 'Polls' Beispiel ...
-# Ebenfalls unklar: Ist die Beziehung Pages-Categories eine "one to many" oder eine "many to many" beziehung?
-
 class product_categories(models.Model):
     category_name = models.CharField(max_length=100)
-    # Should eventually allow the admin to change the order by which the categories are displayed
     category_order = models.IntegerField(default=0)
 
     def __unicode__(self):
@@ -16,19 +12,10 @@ class product_categories(models.Model):
         ordering = ["category_order"]
 
 class page_container(models.Model):
-    # Content of page and it's title. What max_length should be used here?
     page_title = models.CharField('Title', max_length=200)
-    # TextField Info: https://docs.djangoproject.com/en/1.6/ref/models/fields/#django.db.models.TextField
     page_content = models.TextField('Content', max_length=1200)
-    # Number to be used to define order of listed page names
     page_order = models.IntegerField('Order of Page', default=0)
-    # Category ("~ies"?)
     page_categories = models.ForeignKey(product_categories, verbose_name="Category")
-    # Links to Pages containing similar Information
-    """
-    Der nächste Punkt soll Links zu Seiten enthalten, die verwandte Produkte enthalten. Voraussichtlich ist hier der falsche Ort dafür. Ich gehe davon aus, dass es hierfür eine eigene Klasse brauchen wird.
-    Oder nicht. Ein manyToMany auf sich selbst sollte gehen.
-    """
     # page_context_links = models.ManyToManyField("self")
     
     """
