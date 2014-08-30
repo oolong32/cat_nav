f17787db452efd19d7c423972024e48677ec2a9d
Josef Renner <josef.renner@gmail.com>
2014-08-31 00:02:24 +0200

    Replace Product Details View with Generic DetailView
    Change urls.py accordingly. Far from done.

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, url
 
-from cat_nav.views import CategoryList
+from cat_nav.views import CategoryList, PageDetails
 from cat_nav import views
 
 urlpatterns = patterns('',
     url(r'^$', CategoryList.as_view()),
     # url(r'^(?P<page_categories_id>\d+)/$', views.products, name='products'),
-    url(r'^(?P<page_categories_id>\d+)/details/(?P<page_container_id>\d+)/$', views.details, name='details'),
+    url(r'^(?P<page_categories_id>\d+)/(?P<page_container_id>\d+)/$', PageDetails.as_view()),
 )
 """
-Im Tutorial wird unter #4 gezeigt, wie "generic Views" gehen. Habe ich nicht begriffen. - ev. noch nachschlagen und einrichten.
-https://docs.djangoproject.com/en/1.6/topics/class-based-views/
+Der Index sollte alle Kategorien "gefaltet" zeigen.
+Dazu sollte es eine url mit kategorienamen als slug geben, der die jew. kategorie "ausgefaltet" zeigt.
 """
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404 
 
 from cat_nav.models import product_categories, page_container
@@ -6,7 +6,13 @@ from cat_nav.models import product_categories, page_container
 class CategoryList(ListView):
     model = product_categories
 
-def details(request, page_categories_id, page_container_id): 
-    page = get_object_or_404(page_container, pk=page_container_id)
-    return render(request, 'cat_nav/content.html', {'page': page})
+class PageDetails(DetailView):
+    context_object_name = 'product'
+    queryset = page_container.objects.all()
+    # queryset should be filtered for the respective category
+    pk_url_kwarg = 'page_container_id'
+
+# def details(request, page_categories_id, page_container_id): 
+#     page = get_object_or_404(page_container, pk=page_container_id)
+#     return render(request, 'cat_nav/content.html', {'page': page})
 
