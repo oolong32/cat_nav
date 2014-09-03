a608574580e2f1e880f88aaa4f866cfdebd10de8
Josef Renner <josef.renner@gmail.com>
2014-09-03 16:42:08 +0200

    Add Context to Inclusion Tag list_categories
    Add context to inclusion tag as described in documentation under
    https://docs.djangoproject.com/en/1.6/howto/custom-template-tags/#inclusion-tags
    The List now compares the names of the listed category with the name of
    the product's category to determine, which one is active.

--- templatetags/list_categories.py
+++ templatetags/list_categories.py
@@ -2,8 +2,15 @@ from django import template
 from cat_nav.models import product_categories
 register = template.Library()
 
-def list_categories(category):
-    category_list = product_categories.objects.order_by('category_order')
-    return {'category_list': category_list}
+# def list_categories(category):
+#     category_list = product_categories.objects.order_by('category_order')
+#     return {'category_list': category_list}
 
-register.inclusion_tag('cat_nav/list_categories.html')(list_categories)
+def list_categories(context):
+    category_list = product_categories.objects.order_by('category_order')
+    return {
+        'category_list': category_list,
+        'active_category': context['product'],
+    }
+
+register.inclusion_tag('cat_nav/list_categories.html',takes_context=True)(list_categories)
