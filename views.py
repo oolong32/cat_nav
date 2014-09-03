c88eeb4b73d2f068765117711be8e8eb6a6901b2
Josef Renner <josef.renner@gmail.com>
2014-09-03 15:32:50 +0200

    Remove Obsolete Comments
    Remove old view and comments made everything messy etc.

--- views.py
+++ views.py
@@ -1,5 +1,4 @@
 from django.views.generic import ListView, DetailView
-# from django.shortcuts import render, get_object_or_404 
 
 from cat_nav.models import product_categories, page_container
 
@@ -9,12 +8,4 @@ class CategoryList(ListView):
 class PageDetails(DetailView):
     model = page_container
     context_object_name = 'product'
-    # queryset should be filtered for the respective category
-    # that is: '1' must be replaced by 'page_categories_id' but how to access?
-    # or it shouldn't: the category is mainly needed to add a class name to the html in order to highlight the "active" category in the menu.
-    pk_url_kwarg = 'product_id'
-
-# def details(request, page_categories_id, page_container_id): 
-#     page = get_object_or_404(page_container, pk=page_container_id)
-#     return render(request, 'cat_nav/content.html', {'page': page})
-
+    pk_url_kwarg = 'product_id' 
