137d46c51b219e86dccb7348e79d99d90c017798
Josef Renner <josef.renner@gmail.com>
2014-09-04 00:13:14 +0200

    Remove Obsolete Comments in UrlConf
    Remove two obsolete url patterns and some comments.

--- urls.py
+++ urls.py
@@ -1,19 +1,9 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url 
 from cat_nav.views import SimpleCategoryList, CategoryListDeluxe, PageDetails
 from cat_nav import views
 
 urlpatterns = patterns('',
     url(r'^$', SimpleCategoryList.as_view()),
-    # url(r'^(?P<page_categories_id>\d+)/$', views.products, name='products'),
-    # url(r'^(?P<page_categories_id>\d+)/(?P<page_container_id>\d+)/$', PageDetails.as_view()),
     url(r'^categories/(?P<pk>\d+)/$', CategoryListDeluxe.as_view(), name='categories'),
     url(r'^products/(?P<pk>\d+)/$', PageDetails.as_view(), name='products'),
-    #
-    # DetailView needs captured variable to be named "pk" as in tutorial part 4, amend views
-    #
 )
-"""
-Der Index sollte alle Kategorien "gefaltet" zeigen.
-Dazu sollte es eine url mit kategorienamen als slug geben, der die jew. kategorie "ausgefaltet" zeigt.
-"""
