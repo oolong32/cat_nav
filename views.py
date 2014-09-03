90e299ffda57a63c7948ea13cdf2c970e226b35e
Josef Renner <josef.renner@gmail.com>
2014-09-03 18:11:31 +0200

    Add Context to ListView and Apply in Template
    The view generating the category list now checks the url for a pk
    and the template highlights highlights it.
    Now the index doesn't work anymore of course, but we'll get there.

--- views.py
+++ views.py
@@ -4,9 +4,13 @@ from cat_nav.models import product_categories, page_container
 
 class CategoryList(ListView):
     model = product_categories
+    context_object_name = 'all_categories'
+    def get_context_data(self, **kwargs):
+        context = super(CategoryList, self).get_context_data(**kwargs)
+        urlKey = self.kwargs['pk']
+        context['my_category'] = product_categories.objects.get(pk=urlKey)
+        return context
 
 class PageDetails(DetailView):
     model = page_container
-    context_object_name = 'product'
-    # pk_url_kwarg = 'product_id'  <-- not needed when using "pk" in urlConf
-
+    context_object_name = 'product' 
