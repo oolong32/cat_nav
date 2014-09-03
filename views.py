0789c05645e3f5b07ab4b3c14ed149c7a8d4ded0
Josef Renner <josef.renner@gmail.com>
2014-09-04 00:07:31 +0200

    Add separate Views for Category Lists
    I just couldn't figure out how to use one List for two cases, where
    there is or isn't a captured string in the url. Works fine now!

--- views.py
+++ views.py
@@ -2,11 +2,16 @@ from django.views.generic import ListView, DetailView
 
 from cat_nav.models import product_categories, page_container
 
-class CategoryList(ListView):
+class SimpleCategoryList(ListView):
     model = product_categories
     context_object_name = 'all_categories'
+    template_name = 'cat_nav/simple_categories.html'
+
+class CategoryListDeluxe(ListView):
+    model = product_categories
+    context_object_name = 'all_categories_and_some'
     def get_context_data(self, **kwargs):
-        context = super(CategoryList, self).get_context_data(**kwargs)
+        context = super(CategoryListDeluxe, self).get_context_data(**kwargs)
         urlKey = self.kwargs['pk']
         context['my_category'] = product_categories.objects.get(pk=urlKey)
         return context
