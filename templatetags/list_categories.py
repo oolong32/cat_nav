7df6129d005dc6737c67ca73fe5548cffc91c55f
Josef Renner <josef.renner@gmail.com>
2014-09-04 22:34:01 +0200

    Rename Context Variable

--- templatetags/list_categories.py
+++ templatetags/list_categories.py
@@ -10,7 +10,7 @@ def list_categories(context):
     category_list = product_categories.objects.order_by('category_order')
     return {
         'category_list': category_list,
-        'active_category': context['product'],
+        'active_product': context['product'],
     }
 
 register.inclusion_tag('cat_nav/list_categories.html',takes_context=True)(list_categories)
