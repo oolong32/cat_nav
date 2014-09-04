bcaa338300192271bf625b43273d8297329216a3
Josef Renner <josef.renner@gmail.com>
2014-09-04 22:43:13 +0200

    Remove Obsolete Comments

--- templatetags/list_categories.py
+++ templatetags/list_categories.py
@@ -2,10 +2,6 @@ from django import template
 from cat_nav.models import product_categories
 register = template.Library()
 
-# def list_categories(category):
-#     category_list = product_categories.objects.order_by('category_order')
-#     return {'category_list': category_list}
-
 def list_categories(context):
     category_list = product_categories.objects.order_by('category_order')
     return {
