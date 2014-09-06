47b3081a204829c2ca6c195d6ee4c1d97d4d8c2f
Josef Renner <josef.renner@gmail.com>
2014-09-06 15:20:27 +0200

    Move Category Field to Top in Admin Interface

--- admin.py
+++ admin.py
@@ -5,7 +5,7 @@ class PageAdmin(admin.ModelAdmin):
     # page container overview, show categories next to titles
     list_display = ('page_title', 'page_categories', 'page_order')
     # page container edit page: show title and category in same field, then the content.
-    fields = [('page_title', 'page_categories'), 'page_content', 'page_order', 'page_context_links']
+    fields = [('page_title', 'page_categories', 'page_order'), 'page_content', 'page_context_links']
     filter_horizontal = ('page_context_links',)
     # filter pages by categories
     list_filter = ['page_categories']
