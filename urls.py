bfe8f6ea0e993e17df4d09cb28064bb709ee973b
Josef Renner <josef.renner@gmail.com>
2014-09-06 22:51:02 +0200

    Remove Unneccessary Part of Category URL

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url 
+from django.conf.urls import patterns, url
 from cat_nav.views import SimpleCategoryList, CategoryListDeluxe, PageDetails
 from cat_nav import views
 
 urlpatterns = patterns('',
     url(r'^$', SimpleCategoryList.as_view()),
-    url(r'^categories/(?P<pk>\d+)/$', CategoryListDeluxe.as_view(), name='categories'),
+    url(r'^(?P<pk>\d+)/$', CategoryListDeluxe.as_view(), name='categories'),
     url(r'^products/(?P<pk>\d+)/$', PageDetails.as_view(), name='products'),
 )
